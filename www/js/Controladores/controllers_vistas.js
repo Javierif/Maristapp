fb8d9a5d02c394cbd5a323d5c36a059184564a8f
JavierIF <javier.ifer@gmail.com>
2016-12-28 23:15:08 +0100

    Terminado el controlador del login

--- www/js/Controladores/controllers_vistas.js
+++ www/js/Controladores/controllers_vistas.js
@@ -1,6 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('loginCtrl', function ($scope, Peticiones, $state, $ionicLoading) {
+    .controller('loginCtrl', function ($scope, Peticiones, $state, $ionicLoading) {
 
+    $scope.login = function(password) {
+        $ionicLoading.show({
+            template: '<i class="icon ion-looping"></i> Conectando con el servidor...'
+        });
+        var respuesta = Peticiones.login(password);
+        respuesta.then(
+            function (result) {
+                $ionicLoading.hide();
+                if(result.codigo == password) {
+                    $state.go("app.bienvenida");
+                } else {
+                    window.plugins.toast.showShortBottom("código incorrecto.",
+                                                         function (a) {},
+                                                         function (b) {});
+                }
+            },
+            function (errorPlayload) {});
+    }
 
 });
